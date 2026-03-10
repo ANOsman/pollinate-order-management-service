b863a2f76684d153faaae8fbfd4f3e7c41f604d1
ali <anosman6891@gmail.com>
2026-03-10 22:10:12 +0200

    Configure Spring Security

--- src/main/java/com/pollinate/order_management_service/OrderManagementServiceApplication.java
+++ src/main/java/com/pollinate/order_management_service/OrderManagementServiceApplication.java
@@ -2,12 +2,27 @@ package com.pollinate.order_management_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @SpringBootApplication
 public class OrderManagementServiceApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(OrderManagementServiceApplication.class, args);
+	}
+
+	@Bean
+	public InMemoryUserDetailsManager userDetailsService(BCryptPasswordEncoder encoder) {
+		UserDetails admin = User.withUsername("admin")
+				.password(encoder.encode("password"))
+				.roles("ADMIN")
+				.build();
+		return new InMemoryUserDetailsManager(admin);
 	}
 
 }
---
+++ src/main/java/com/pollinate/order_management_service/config/SecurityConfig.java
@@ -0,0 +1,42 @@
+package com.pollinate.order_management_service.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.core.userdetails.*;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .headers(headers -> headers.frameOptions(
+                        HeadersConfigurer.FrameOptionsConfig::sameOrigin))
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(
+                                "/h2-console/**",
+                                "/swagger-ui/**",
+                                "/swagger-ui.html",
+                                "/v3/api-docs/**"
+                        ).permitAll()
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(Customizer.withDefaults());
+
+        return http.build();
+    }
+
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
